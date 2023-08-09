013c709890f19243b23c013dbddde934d3e047b3
JackM0N <przemyslawgotowala@gmail.com>
2023-08-09 17:13:56 +0200

    Add Chroma case

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import noCase as nc
 import dreamsAndNightmares as dan
 import riptide as rip
 import fracture as fr
+import chroma as ch
 
 print('')
 print("Hello! Welcome to my CS:GO Case Opening Simulator!")
@@ -10,6 +11,9 @@ print("0. Generic case (No skin names, just rarity and wear)")
 print("1. Dreams & Nightmares Case")
 print("2. Riptide")
 print("3. Fracture")
+print("4. Chroma")
+print("5. Chroma 2")
+print("6. Chroma 3")
 print('')
 
 while True:
@@ -30,6 +34,8 @@ elif choice == 2:
     rip.run()
 elif choice == 3:
     fr.run()
+elif choice == 4:
+    ch.run()
 
-#Future idea: make terminal output display correct rarity color!
+
 #Future idea: make it posible to input values of keys and chests to display how much money was spent (Maybe even scrape Steam values of chests and skins from marketplace)
